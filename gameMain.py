7cfd64c4712f7b6b3e35a822cd6f0e4cc1676848
kamdoescode <kamryn.hanley@gmail.com>
2025-01-14 10:15:28 +0000

    added rich library for improved terminal output

--- gameMain.py
+++ gameMain.py
@@ -1,2 +1,16 @@
-#instruction display
-print("Welcome to codebreaker")
+import rich
+from rich import print
+from rich.panel import Panel 
+from rich.text import Text
+
+instructions = Text.assemble(
+    ("Welcome to Code Breaker!", "bold magenta"),
+    "\n\nPlayer 1: Create a secret code",
+    "\nPlayer 2: Try to guess the code",
+    "\n\nGoodluck!"
+)
+
+print(Panel(instructions, title = "Game Instructions",
+border_style= "bright_blue"))
+
+
