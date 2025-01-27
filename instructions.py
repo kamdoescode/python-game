909b6064d511340cb09e99dcdc8fe950b041bd49
kamdoescode <kamryn.hanley@gmail.com>
2025-01-27 13:27:32 +0000

    updating instructions

--- instructions.py
+++ instructions.py
@@ -10,7 +10,7 @@ def display_instructions():
         ("Welcome to Code Breaker!", "bold magenta"),
         "\n\n\n1.Assign a Player 1 and a Player 2"
         "\n\n2.Each player create a 5 digit code of letters and numbers"
-        "\n\n3.Player 2, try to guess the code"
+        "\n\n3.Players, take turns guessing the code"
         "\n>>>A guess of the right character in the right position will turn Green"
         "\n>>>A guess of the wrong character in wrong position will turn Red"
         "\n\n5.Whoever guesses the code first wins"
